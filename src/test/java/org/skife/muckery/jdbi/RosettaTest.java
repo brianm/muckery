54238e3a4162456da58d314963aba005a77ce278
Brian McCallister <brianm@skife.org>
2016-11-08 10:23:04 -0800

    rosetta stuff

--- src/test/java/org/skife/muckery/jdbi/RosettaTest.java
+++ src/test/java/org/skife/muckery/jdbi/RosettaTest.java
@@ -8,9 +8,17 @@ import com.hubspot.rosetta.jdbi.RosettaObjectMapperOverride;
 import org.junit.Rule;
 import org.junit.Test;
 import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterContainerMapper;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,8 +29,6 @@ public class RosettaTest {
     @Test
     public void testFoo() throws Exception {
         final DBI dbi = new DBI(this.h2);
-        dbi.registerMapper(new RosettaMapperFactory());
-        dbi.registerContainerFactory(new OptionalContainerFactory());
         new RosettaObjectMapperOverride(new ObjectMapper().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES))
                 .override(dbi);
 
@@ -34,6 +40,13 @@ public class RosettaTest {
                       .isEqualTo(new Something(1, "Happy"));
     }
 
+    @Test
+    public void testOverrideMapper() throws Exception {
+
+    }
+
+    @RegisterContainerMapper(OptionalContainerFactory.class)
+    @RegisterMapperFactory(RosettaMapperFactory.class)
     private interface Dao {
         @SqlUpdate("create table something (id int primary key, name varchar)")
         void migrate();
@@ -43,5 +56,18 @@ public class RosettaTest {
 
         @SqlQuery("select id, name from something where id = :id")
         Something findById(@Bind("id") int id);
+
+
+        @SqlQuery("select id, name from something where id = :id")
+        @Mapper(SpecialMapper.class)
+        Something findById2(@Bind("id") int id);
+    }
+
+    public static class SpecialMapper implements ResultSetMapper<Something> {
+
+        @Override
+        public Something map(final int i, final ResultSet resultSet, final StatementContext statementContext) throws SQLException {
+            return null;
+        }
     }
 }
---
+++ src/test/java/org/skife/muckery/ztzip/TestZtZip.java
@@ -0,0 +1,10 @@
+package org.skife.muckery.ztzip;
+
+import org.junit.Test;
+
+public class TestZtZip {
+    @Test
+    public void testZippery() throws Exception {
+
+    }
+}
